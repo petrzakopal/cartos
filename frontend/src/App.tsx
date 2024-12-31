431ce95db1ca4ee82e382ad11042461c3d2c10a1
Petr Zakopal <petr.zakopal@gmail.com>
2024-12-31 13:31:05 +0100

    frontend: prepared plain vite react frontend with tailwindcss
    prefered runtime: bun

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className='w-full relative flex flex-col items-center'>
+                <h1 className='text-black font-bold'>Cartos</h1>
+                <span className="italic text-blue-400">under development</span>
+      </div>
+    </>
+  )
+}
+
+export default App
